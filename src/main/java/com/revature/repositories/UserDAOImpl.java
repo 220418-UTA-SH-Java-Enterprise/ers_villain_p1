36fd3215cef3672f175e349bc4bf2457c341ade5
FullSol <ceraines@student.fullsail.edu>
2022-05-10 06:58:12 -0400

    updated log4j to the most recent version.

--- src/main/java/com/revature/repositories/UserDAOImpl.java
+++ src/main/java/com/revature/repositories/UserDAOImpl.java
@@ -1,51 +1,55 @@
-mport java.sql.Connection;
+package com.revature.repositories;
+
+import java.sql.Connection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.revature.exceptions.InvalidLoginAttempt;
 import com.revature.models.User;
 import com.revature.util.ConnectionUtil;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class UserDAOImpl implements UserDAO {
+
+  private Logger logger = LogManager.getLogger(UserDAOImpl.class);
 
   @Override
   public boolean insert(User user) {
     try (Connection conn = ConnectionUtil.getConnection()) {
-            logger.info("In DAO layer: making a new user in db...");
-            String sql = "INSERT INTO users (username, password, email, first_name, last_name, role_id) VALUES (?, ?, ?, ?, ?, ?)";
+      logger.info("In DAO layer: making a new user in db...");
+      String sql = "INSERT INTO users (username, password, email, first_name, last_name, role_id) VALUES (?, ?, ?, ?, ?, ?)";
 
-            PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, user.getUsername());
-            stmt.setString(2, user.getPassword());
-            stmt.setString(3, user.getEmail());
-            stmt.setString(4, user.getFirstName());
-            stmt.setString(5, user.getLastName());
-            stmt.setInt(6, user.getRoleId());
+      PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+      stmt.setString(1, user.getUsername());
+      stmt.setString(2, user.getPassword());
+      stmt.setString(3, user.getEmail());
+      stmt.setString(4, user.getFirstName());
+      stmt.setString(5, user.getLastName());
+      stmt.setInt(6, user.getRoleId());
 
-            stmt.execute();
+      stmt.execute();
 
-            ResultSet rs = stmt.getGeneratedKeys();
+      ResultSet rs = stmt.getGeneratedKeys();
 
-            int genKey = 0;
-            if (rs.next()) {
-                genKey = rs.getInt(1);
-            }
+      int genKey = 0;
+      if (rs.next()) {
+        genKey = rs.getInt(1);
+      }
 
-            user.setId(genKey);
+      user.setId(genKey);
 
-            logger.info("New user has been entered into the database.");
+      logger.info("New user has been entered into the database.");
 
-            conn.close();
-        } catch (SQLException e) {
-            logger.warn("Unable to execute SQL statement:", e);
-            return false;
-        }
-        return true;
+      conn.close();
+    } catch (SQLException e) {
+      logger.warn("Unable to execute SQL statement:", e);
+      return false;
+    }
+    return true;
   }
 
   @Override
@@ -65,5 +69,5 @@ public class UserDAOImpl implements UserDAO {
     // TODO Auto-generated method stub
     return null;
   }
-  
+
 }
