f92fcfe4891b19c06f6b08069bcde3d6d6173d43
FullSol <ceraines@student.fullsail.edu>
2022-05-10 23:07:26 -0400

    SQL update, added ER diagram, Reimb insert method completed.

--- Script-4.sql
+++ Script-4.sql
@@ -1,28 +1,31 @@
-DROP TABLE IF EXISTS ers_reimbursement_status;
-DROP TABLE IF EXISTS ers_reimbursement_type;
-DROP TABLE IF EXISTS ers_user_roles;
+DROP TABLE IF EXISTS ers_reimbursements;
 DROP TABLE IF EXISTS ers_users;
-DROP TABLE IF EXISTS ers_reimbursement;
+DROP TABLE IF EXISTS ers_reimbursement_type;
+DROP TABLE IF EXISTS ers_reimbursement_status;
+DROP TABLE IF EXISTS ers_user_roles;
+
+
+
 
 CREATE TABLE ers_reimbursement_status(
-	reimb_status_id serial CONSTRAINT reimb_status_pk PRIMARY KEY,
+	status_id serial CONSTRAINT reimb_status_pk PRIMARY KEY,
 	reimb_type_id int
 );
 
 CREATE TABLE ers_reimbursement_type (
-  reimb_type_id serial CONSTRAINT reimb_type_pk PRIMARY KEY, 
+  type_id serial CONSTRAINT reimb_type_pk PRIMARY KEY, 
   reimb_type VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE ers_user_roles(
-	ers_user_role_id serial CONSTRAINT user_role_pk PRIMARY KEY,
+	user_role_id serial CONSTRAINT user_role_pk PRIMARY KEY,
 	user_role int UNIQUE NOT NULL
 );
 
 CREATE TABLE ers_users(
-	ers_user_id serial CONSTRAINT user_pk PRIMARY KEY,
-	ers_username varchar(50) UNIQUE NOT NULL,
-	ers_password varchar(50) NOT NULL,
+	user_id serial CONSTRAINT user_pk PRIMARY KEY,
+	username varchar(50) UNIQUE NOT NULL,
+	password varchar(50) NOT NULL,
 	user_first_name varchar(100) NOT NULL,
 	user_last_name varchar(100) NOT NULL,
 	user_email varchar(150) UNIQUE NOT NULL,
@@ -35,31 +38,31 @@ CREATE TABLE ers_users(
 
 CREATE TABLE ers_reimbursements(
 	reimb_id serial CONSTRAINT reimb_pk PRIMARY KEY,
-	reimb_amount decimal(10,2) NOT NULL,
-	reimb_submitted date NOT NULL,
-	reimb_resolved date,
-	reimb_description varchar(250),
-	reimb_receipt bytea,
-	reimb_author int NOT NULL,
-	reimb_resolver int,
-	reimb_status_id int NOT NULL,
-	reimb_type_id int NOT NULL,
+	amount decimal(10,2) NOT NULL,
+	submitted date NOT NULL,
+	resolved date,
+	description varchar(250),
+	receipt bytea,
+	author int NOT NULL,
+	resolver int,
+	status_id int NOT NULL,
+	type_id int NOT NULL,
 
-CONSTRAINT ers_user_fk_auth -- name from physical model chart
-FOREIGN KEY (reimb_author) -- name of column in current tbale
-REFERENCES ers_users(ers_user_id), -- name of table (and column) in foreign table
+CONSTRAINT user_fk_auth -- name from physical model chart
+FOREIGN KEY (author) -- name of column in current tbale
+REFERENCES ers_users(user_id), -- name of table (and column) in foreign table
 
-CONSTRAINT ers_user_fk_resolver -- name from physical model chart
-FOREIGN KEY (reimb_resolver) -- name of column in current tbale
-REFERENCES ers_users(ers_user_id), -- name of table (and column) in foreign table
+CONSTRAINT user_fk_resolver -- name from physical model chart
+FOREIGN KEY (resolver) -- name of column in current tbale
+REFERENCES ers_users(user_id), -- name of table (and column) in foreign table
 
-CONSTRAINT ers_reimb_status_fk -- name from physical model chart
-FOREIGN KEY (reimb_status_id) -- name of column in current tbale
-REFERENCES ers_reimbursement_status(reimb_status_id), -- name of table (and column) in foreign table
+CONSTRAINT reimb_status_fk -- name from physical model chart
+FOREIGN KEY (status_id) -- name of column in current tbale
+REFERENCES ers_reimbursement_status(status_id), -- name of table (and column) in foreign table
 
 CONSTRAINT ers_reimb_type_fk -- name from physical model chart
-FOREIGN KEY (reimb_type_id) -- name of column in current table
-REFERENCES ers_reimbursement_type(reimb_type_id) -- name of table (and column) in foreign table
+FOREIGN KEY (type_id) -- name of column in current table
+REFERENCES ers_reimbursement_type(type_id) -- name of table (and column) in foreign table
 
 );
 
--- src/main/java/com/revature/repositories/ReimbDAOImpl.java
+++ src/main/java/com/revature/repositories/ReimbDAOImpl.java
@@ -1,13 +1,65 @@
 package com.revature.repositories;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.revature.models.Reimb;
+import com.revature.util.ConnectionUtil;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ReimbDAOImpl implements ReimbDAO {
 
+    private static Logger logger = LogManager.getLogger(ReimbDAOImpl.class);
+
     @Override
     public boolean insert(Reimb reimb) {
-        // TODO Auto-generated method stub
-        return false;
+        try (Connection conn = ConnectionUtil.getConnection()) {
+            logger.info("In DAO layer: making a reimbursement in db...");
+
+            // SQL Statement to be executed for addition to the reimbursement table
+            String sql = "INSERT INTO ers_reimbursements (amount, submitted, resolved, description, receipt, author, resolver, status_id, type_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+            // Create the prepared statment variable
+            PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            // Update fields with data from the model
+            stmt.setDouble(1, reimb.getAmount());
+            stmt.setDate(2, Date.valueOf(reimb.getSubmitted()));
+            stmt.setDate(3, Date.valueOf(reimb.getResolved()));
+            stmt.setString(4, reimb.getDescription());
+            stmt.setString(5, reimb.getDescription());
+            stmt.setInt(6, reimb.getAuthorId());
+            stmt.setInt(7, reimb.getResolverId());
+            stmt.setInt(8, reimb.getStatusid());
+            stmt.setInt(9, reimb.getTypeId());
+
+            // Execute the statement
+            stmt.execute();
+
+            // Retrieve the newly created field
+            ResultSet rs = stmt.getGeneratedKeys();
+
+            int genKey = 0;
+            if (rs.next()) {
+                // If a new user was created this will return the reimb_id column of that field
+                genKey = rs.getInt("reimb_id");
+            }
+
+            // This will update the reim model with the auto generated id
+            reimb.setUserId(genKey);
+
+            logger.info("New reimbursement has been entered into the database.");
+        } catch (SQLException e) {
+            logger.warn("Unable to execute SQL statement: " + e.getMessage(), e);
+            return false;
+        }
+        return true;
     }
 
     @Override
