36d0e597249c24916a329f7551afa3e896f4a548
L3viathon <lvmchoi@gmail.com>
2022-05-11 07:22:28 -0700

    Added update and findById methods to UserDAOImpl

--- src/main/java/com/revature/repositories/UserDAOImpl.java
+++ src/main/java/com/revature/repositories/UserDAOImpl.java
@@ -19,15 +19,15 @@ public class UserDAOImpl implements UserDAO {
   @Override
   public boolean insert(User user) {
     try (Connection conn = ConnectionUtil.getConnection()) {
-      logger.info("In DAO layer: making a new user in db...");
-      String sql = "INSERT INTO users (username, password, email, first_name, last_name, role_id) VALUES (?, ?, ?, ?, ?, ?)";
+      logger.info("In DAO layer: making a new user in database");
+      String sql = "INSERT INTO ers_users (ers_username, ers_password, user_first_name, user_last_name, user_email, user_role_id) VALUES (?, ?, ?, ?, ?, ?)";
 
       PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
       stmt.setString(1, user.getUsername());
       stmt.setString(2, user.getPassword());
-      stmt.setString(3, user.getEmail());
-      stmt.setString(4, user.getFirstName());
-      stmt.setString(5, user.getLastName());
+      stmt.setString(3, user.getFirstName());
+      stmt.setString(4, user.getLastName());
+      stmt.setString(5, user.getEmail());
       stmt.setInt(6, user.getRoleId());
 
       stmt.execute();
@@ -53,22 +53,66 @@ public class UserDAOImpl implements UserDAO {
 
   @Override
   public boolean update(User user) {
-    // TODO: Add update user method
-    return false;
+    logger.info("In DAO layer: Updating user information by id...");
+
+    try (Connection conn = ConnectionUtil.getConnection()) {
+      String sql = "UPDATE ers_users SET ers_username = ?, ers_password = ?, user_first_name = ?, user_last_name = ?, user_email = ?, user_role_id = ? WHERE ers_user_id = ?;";
+
+      PreparedStatement stmt = conn.prepareStatement(sql);
+      stmt.setString(1, user.getUsername());
+      stmt.setString(2, user.getPassword());
+      stmt.setString(3, user.getFirstName());
+      stmt.setString(4, user.getLastName());
+      stmt.setString(5, user.getEmail());
+      stmt.setInt(6, user.getRoleId());
+      stmt.setInt(7, user.getUserId());
+
+      logger.info("User update by ID " + user.getUserId() + " was successful. ");
+
+    } catch (SQLException e) {
+      logger.warn("Unable to execute SQL statement", e);
+      return false;
+    }
+
+    return true;
   }
 
   @Override
   public User findById(int id) {
-    // TODO: add findById method
-    return null;
+    logger.info("In DAO layer: Getting user by ID...");
+    User user = new User();
+
+    try (Connection conn = ConnectionUtil.getConnection()) {
+      String sql = "SELECT * FROM ers_users WHERE ers_user_id = ?;";
+      PreparedStatement stmt = conn.prepareStatement(sql);
+      stmt.setInt(1, id);
+      ResultSet rs = stmt.executeQuery();
+
+      if (rs.next()) {
+        user.setUserId(rs.getInt("ers_user_id"));
+        user.setUsername(rs.getString("ers_username"));
+        // user.setPassword(rs.getString("ers_password")); // Don't return the
+        // password...
+        user.setFirstName(rs.getString("user_first_name"));
+        user.setLastName(rs.getString("user_last_name"));
+        user.setEmail(rs.getString("user_email"));
+        user.setRoleId(rs.getInt("user_role_id"));
+      }
+
+      logger.info("User search by id was successful. " + id);
+    } catch (SQLException e) {
+      logger.warn("Unable to execute SQL statement", e);
+    }
+
+    return user;
   }
 
   @Override
   public User login(String username, String password) {
-    logger.info("In DAO Layer: UserDAOImpl() - attemptint to login user.");
+    logger.info("In DAO Layer: UserDAOImpl() - attempting to login user.");
     User user = new User();
     try (Connection conn = ConnectionUtil.getConnection()) {
-      String sql = "SELECT * from users where username = ? AND password = ?;";
+      String sql = "SELECT * FROM ers_users where ers_username = ? AND ers_password = ?;";
 
       PreparedStatement stmt = conn.prepareStatement(sql);
       stmt.setString(1, username);
@@ -77,14 +121,16 @@ public class UserDAOImpl implements UserDAO {
       ResultSet rs = stmt.executeQuery();
 
       if (rs.next()) {
-        user.setUserId(rs.getInt("id"));
-        user.setUsername(rs.getString("username"));
-        // user.setPassword(rs.getString("password")); // Don't return the password...
-        user.setFirstName(rs.getString("first_name"));
-        user.setLastName(rs.getString("last_name"));
-        user.setEmail(rs.getString("email"));
-        user.setRoleId(rs.getInt("role_id"));
+        user.setUserId(rs.getInt("ers_user_id"));
+        user.setUsername(rs.getString("ers_username"));
+        // user.setPassword(rs.getString("ers_password")); // Don't return the password...
+        user.setFirstName(rs.getString("user_first_name"));
+        user.setLastName(rs.getString("user_last_name"));
+        user.setEmail(rs.getString("user_email"));
+        user.setRoleId(rs.getInt("user_role_id"));
       }
+      logger.info("Login user by username and password was successful.");
+
     } catch (SQLException e) {
       logger.warn("Unable to execute query");
       return null;
