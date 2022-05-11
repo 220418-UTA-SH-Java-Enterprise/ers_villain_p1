01c1c32df62df6bd5c7227cfd8ad15c3e139b079
FullSol <ceraines@student.fullsail.edu>
2022-05-10 21:54:51 -0400

    created ReimbDao with notes to user stories.

---
+++ src/main/java/com/revature/repositories/ReimbDAO.java
@@ -0,0 +1,20 @@
+package com.revature.repositories;
+
+import com.revature.models.Reimb;
+
+public interface ReimbDAO {
+    // Submit new reimbursement requests to
+    public boolean insert(Reimb reimb);
+
+    // Change anything about Reimbursement
+    public boolean update(Reimb reimb);
+
+    // View single Reimbursement request
+    public Reimb findById(int id);
+
+    // View all request of single user
+    public <List> Reimb findByUserId(int userId);
+
+    // View all requests by status type
+    public <List> Reimb findAll(int statusTypeId);
+}
---
+++ src/main/java/com/revature/repositories/ReimbDAOImpl.java
@@ -0,0 +1,37 @@
+package com.revature.repositories;
+
+import com.revature.models.Reimb;
+
+public class ReimbDAOImpl implements ReimbDAO {
+
+    @Override
+    public boolean insert(Reimb reimb) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean update(Reimb reimb) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Reimb findById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public <List> Reimb findByUserId(int userId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public <List> Reimb findAll(int statusTypeId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
