929aa600fcb3a831a48b7e0a12feef25f9127921
L3viathon <lvmchoi@gmail.com>
2022-05-11 06:06:36 -0700

    Added UserRole model.

---
+++ src/main/java/com/revature/models/UserRole.java
@@ -0,0 +1,65 @@
+package com.revature.models;
+
+public class UserRole {
+
+    private int roleId;
+    private String role;
+
+    public UserRole() {
+    }
+
+    public UserRole(int roleId, String role) {
+        this.roleId = roleId;
+        this.role = role;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole [role=" + role + ", roleId=" + roleId + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((role == null) ? 0 : role.hashCode());
+        result = prime * result + roleId;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserRole other = (UserRole) obj;
+        if (role == null) {
+            if (other.role != null)
+                return false;
+        } else if (!role.equals(other.role))
+            return false;
+        if (roleId != other.roleId)
+            return false;
+        return true;
+    }
+
+}
