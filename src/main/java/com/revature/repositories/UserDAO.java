3b1165fd8c05b6aa939be70435c733b63cf1ccd7
FullSol <ceraines@student.fullsail.edu>
2022-05-13 18:56:36 -0400

    testing connection.

--- src/main/java/com/revature/repositories/UserDAO.java
+++ src/main/java/com/revature/repositories/UserDAO.java
@@ -7,8 +7,12 @@ import com.revature.models.User;
 
 public interface UserDAO {
   public boolean insert(User user);
-  public boolean update(User user);
+
+  public User update(User user);
+
   public User findById(int id);
+
   public ArrayList<User> findAllUsers();
+
   public User login(String username, String password);
 }
